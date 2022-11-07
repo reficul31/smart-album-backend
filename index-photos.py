94d22a0a313482d256f41217ca07d94c45aed145
reficul31 <reficul31@gmail.com>
2022-11-07 12:32:25 -0500

    Adding authentication

--- index-photos.py
+++ index-photos.py
@@ -3,11 +3,10 @@ import requests
 
 from datetime import datetime
 
-AWSAUTH = ('', '')
+AWSAUTH = ('master', 'Columbia110x3$')
 HEADERS = { "Content-Type": "application/json" }
-CUSTOM_LABEL_FIELD_NAME = 'x-amz-meta-customLabels'
-
-URL = ""
+CUSTOM_LABEL_FIELD_NAME = 'customlabels'
+URL = "https://search-photos-ctm5peqt7woxp4qzhpvenm3p7y.us-east-1.es.amazonaws.com/photos-index/_doc"
 
 def lambda_handler(event, context):
     print("Event: ", event)
@@ -17,19 +16,22 @@ def lambda_handler(event, context):
         
         client = boto3.client('s3')
         response = client.head_object(Bucket=bucket, Key=photo)
+        
+        print("Head Object: {}".format(response["Metadata"]))
         labels, metadata = [], response['Metadata']
         if CUSTOM_LABEL_FIELD_NAME in metadata:
             print("Metadata: ", metadata)
-            labels.append(metadata[CUSTOM_LABEL_FIELD_NAME])
+            labels = [label.lower() for label in metadata[CUSTOM_LABEL_FIELD_NAME].split(",")]
         else:
             print("No custom labels for the image found")
         
         client = boto3.client('rekognition')
         response = client.detect_labels(Image={'S3Object': {'Bucket': bucket, 'Name': photo}}, MaxLabels=10)
-        labels = labels + [label['Name'] for label in response['Labels']]
+        labels = labels + [label['Name'].lower() for label in response['Labels']]
         
         createdTimestamp = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
         data = {'objectKey': photo, 'bucket': bucket, 'createdTimestamp': createdTimestamp, 'labels': labels}
         print("Data: {}".format(data))
 
-        response = requests.post(URL, auth=AWSAUTH, json=data, header=HEADERS)
+        response = requests.post(URL, auth=AWSAUTH, json=data, headers=HEADERS)
+        print("Response:", response)
